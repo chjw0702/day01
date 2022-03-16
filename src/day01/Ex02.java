f50d3ddfe9596aa69de5c68d4e692f6701124337
Your Name <you@example.com>
2022-03-16 14:30:21 +0900

    Ex02

---
+++ src/day01/Ex02.java
@@ -0,0 +1,48 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		System.out.println( 100 + 100 );
+		System.out.println( 100.123 + 100);
+		
+		System.out.println("백" + 100+100);
+		System.out.println("백" + 100+"\t"+100);
+		System.out.println("백100100 위랑같음");
+		System.out.println("100" + "100");
+		
+		
+		
+		System.out.println("========================================");
+		System.out.println("이름\t나이\t전화번호\t\t회비");
+		System.out.println("========================================");
+		System.out.print("홍길동\t");
+		System.out.print("\"");
+		System.out.print(15);
+		System.out.print("\"");
+		System.out.print("\t");
+		System.out.print(3672+"-"+1234);
+		System.out.print("\t");
+		System.out.println("\\"+20000);
+		System.out.print("고길동\t");
+		System.out.print("\"");
+		System.out.print(15);
+		System.out.print("\"");
+		System.out.print("\t");
+		System.out.print(2238+"-"+1234);
+		System.out.print("\t");
+		System.out.println("\\"+30000);
+		System.out.print("김말이\t");
+		System.out.print("\"");
+		System.out.print(15);
+		System.out.print("\"");
+		System.out.print("\t");
+		System.out.print(1234+"-"+1234);
+		System.out.print("\t");
+		System.out.println("\\"+50000);
+		System.out.println("----------------------------------------");
+		System.out.print("총합계\t\t\t\t");
+		System.out.println("\\"+100000);
+		System.out.println("----------------------------------------");
+
+	}
+}
